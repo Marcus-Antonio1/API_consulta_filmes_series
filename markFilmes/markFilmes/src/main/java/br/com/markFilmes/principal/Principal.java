1b7cc8997ec4e1ceee56d6e2805849cce880d192
Marcus Antônio <marcustoledo26@gmail.com>
2026-04-22 10:55:43 -0300

    Adicionando estatiscas dos episódios

--- markFilmes/markFilmes/src/main/java/br/com/markFilmes/principal/Principal.java
+++ markFilmes/markFilmes/src/main/java/br/com/markFilmes/principal/Principal.java
@@ -7,10 +7,7 @@ import br.com.markFilmes.service.ConverteDados;
 import javax.sound.midi.Soundbank;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -69,6 +66,19 @@ public class Principal {
                     ).collect(Collectors.toList());
             episodios.forEach(System.out::println);
 
+            System.out.println("Digite um trecho do titulo do episódio: ");
+            var trechoTitulo = leitura.nextLine();
+            Optional<Episodio> episodioBuscado = episodios.stream()
+                    .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                    .findFirst();
+            if (episodioBuscado.isPresent()){
+                System.out.println("Episódio encontrado: ");
+                System.out.println("Temporada " + episodioBuscado.get().getTemporada());
+            } else {
+                System.out.println("Episódio não encontrado!");
+            }
+
+
             System.out.println("A partir de qual ano você deseja ver os episódios? ");
             var ano = Integer.parseInt(leitura.nextLine());
             leitura.nextLine();
@@ -85,7 +95,19 @@ public class Principal {
                         );
                     });
 
+            Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                    .filter(e -> e.getAvaliacao() > 0.0)
+                    .collect(Collectors.groupingBy(Episodio::getTemporada,
+                            Collectors.averagingDouble(Episodio::getAvaliacao)));
+            System.out.println(avaliacoesPorTemporada);
 
+            DoubleSummaryStatistics est = episodios.stream()
+                    .filter(e -> e.getAvaliacao() > 0.0)
+                    .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+            System.out.println("Média: " + est.getAverage());
+            System.out.println("Melhor episódio: " + est.getMax());
+            System.out.println("Pior episódio: " + est.getMin());
+            System.out.println("Quantidade de episódios avaliados: " + est.getCount());
 
 
         } else if (geral.tipo().equalsIgnoreCase("movie")) {
